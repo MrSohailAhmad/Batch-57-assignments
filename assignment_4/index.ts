40564d8aa7f2d01bf17735c4cdb62dbeaed34979
MrSohailAhmad <kmuhammadsohail7@gmail.com>
2024-04-08 00:56:05 +0500

    Declare and print greeting variable

---
+++ assignment_4/index.ts
@@ -0,0 +1,5 @@
+//? Hello Variable
+//? Instructions: Declare a variable named greeting with the string value "Hello, World!" and print it.
+let greeting: string = "Hello, World!"; // Assign "Hello, World!" to this variable and print it.
+
+console.log(greeting);
