8e9ab5bcca78bf1f07ae0d6eef9f13c32ed929c3
MrSohailAhmad <kmuhammadsohail7@gmail.com>
2024-04-08 00:58:08 +0500

    Perform basic arithmetic operations on two numbers

--- assignment_4/index.ts
+++ assignment_4/index.ts
@@ -1,5 +1,23 @@
 //? Hello Variable
 //? Instructions: Declare a variable named greeting with the string value "Hello, World!" and print it.
-let greeting: string = "Hello, World!"; // Assign "Hello, World!" to this variable and print it.
+// let greeting: string = "Hello, World!"; // Assign "Hello, World!" to this variable and print it.
 
-console.log(greeting);
+// console.log(greeting);
+
+//? 2. Basic Math
+//? Instructions: Define two variables with integer values and calculate their sum, difference, product, and quotient.
+
+let num1: number = 5,
+  num2: number = 9; // Assign integer values and perform arithmetic operations.
+
+let result: number = num1 + num2; // * SUM
+console.log("Sum of num1 and num2 is : ", result);
+
+result = num1 - num2; // * Difference
+console.log("Difference of num1 and num2 is : ", result);
+
+result = num1 / num2; // * Quotient
+console.log("Quotient of num1 and num2 is : ", result);
+
+result = num1 * num2; // * Product
+console.log("Product of num1 and num2 is : ", result);
